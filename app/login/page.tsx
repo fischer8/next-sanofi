a2e4817e1c06722154c22f917b9c714623f80238
fischer8 <fischxr@hotmail.com>
2023-09-16 13:19:04 -0300

    dev

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -10,32 +10,37 @@ interface Info {
 export default function Login() {
   const [info, setInfo] = useState<Info>({ crm: 0, uf: "" });
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLSelectElement>) => {
     const { value, id } = event.target;
-    setInfo((prevInfo) => ({
-      ...prevInfo,
+    setInfo((old) => ({
+      ...old,
       [id]: value,
     }));
   };
 
-  const handleSubmit = async (e: React.FormEvent) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const status = await verify(info.uf, info.crm);
     if (status === "Ativo") {
-      alert(status) 
+      alert(status)
     }
   }
 
   return (
-    <form onSubmit={handleSubmit} className='min-h-screen bg-white w-full h-full text-black'>
-      <label htmlFor="crm">CRM</label>
-      <input required id='crm' type="number" onChange={handleChange} value={info.crm === 0 ? '' : info.crm} />
-      <label htmlFor="uf">UF</label>
-      <input required id='uf' type="text" onChange={handleChange} value={info.uf} />
-      <button type='submit'>
-        test
-      </button>
-    </form>
+    <main className="absolute w-full top-0 bottom-0 md:relative md:min-h-screen flex justify-center items-center bg-white">
+      <form onSubmit={handleSubmit} className='text-black p-16 border rounded mx-auto flex flex-col justify-between'>
+          <label htmlFor="crm">CRM</label>
+          <input className="mb-5 border w-3/6" required id='crm' type="number" onChange={handleChange} value={info.crm === 0 ? '' : info.crm} />
+          <label htmlFor="uf">UF</label>
+          <select className="rounded p-1 w-3/12 mb-5" onChange={handleChange} value={info.uf} required id="uf">
+            <option selected value="SP">SP</option>
+            <option value="MG">MG</option>
+          </select>
+        <button className="border rounded bg-purple-500 text-white p-2" type='submit'>
+          test
+        </button>
+      </form>
+    </main>
   );
 }
 
