90a458b117e7a55ff565b36388414bb8bec395bb
fischer8 <fischxr@hotmail.com>
2023-09-23 22:47:31 -0300

    dev

--- components/Menu/info.tsx
+++ components/Menu/info.tsx
@@ -1,7 +1,7 @@
 const cardInfo = [
   { id: 0, title: 'Avaliação de Caprini', desc: 'Calcula o risco de TEV em pacientes cirúrgicos', link: 'caprini', img: '/avaliacao.jpg' },
   { id: 1, title: 'Cáculo de Dose', desc: 'Dose para tratamento do tromboembolismo', link: 'dose', img: '/microscopio.jpg' },
-  { id: 2, title: 'Escore de Pádua', desc: 'oi', link: 'padua', img: '/especiais.jpg' },
+  { id: 2, title: 'Escore de Pádua', desc: 'test', link: 'padua', img: '/especiais.jpg' },
   { id: 3, title: 'Profilaxia', desc: ' test', link: 'profilaxia', img: '/profilaxia.jpg' },
 ];
 
