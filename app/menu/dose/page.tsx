934465da9ef1ec1ef3c0ec2bedb5e4a6f8d062ae
fischer8 <fischxr@hotmail.com>
2023-09-23 22:49:44 -0300

    dev

--- app/menu/dose/page.tsx
+++ app/menu/dose/page.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export default function Dose() {
   return (
     <main>
-      <section className="lg:bg-purple-300 flex items-center p-2 ps-4 pe-6">
+      <section className="bg-purple-300 flex items-center p-2 ps-4 pe-6">
         <Image src="/hamb_icon.webp" width={50} height={90} alt="Menu" />
         <h1 className="p-5 font-bold text-center mx-auto">
           Dose para tratamento do tromboembolismo venoso com ou sem embolia pulmonar
