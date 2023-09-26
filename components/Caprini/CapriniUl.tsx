bb16a1ef58374077a0eea848f45521af67cf0b96
fischer8 <fischxr@hotmail.com>
2023-09-26 15:02:41 -0300

    dev

--- components/Caprini/CapriniUl.tsx
+++ components/Caprini/CapriniUl.tsx
@@ -7,7 +7,7 @@ export default function CapriniUl({ handleScore, score, menuPage }: CapriniProp)
     const isChecked = score.some((s) => s.id === id);
     return (
       <li className="mb-1 md:mb-0 md:hover:bg-purple-100 rounded border flex flex-row justify-between" key={id}>
-        <input onChange={() => handleScore({ id, value })} className="appearance-none rounded-l border w-1/12 cursor-pointer me-px md:me-2 checked:bg-purple-500" checked={isChecked} id={`inp-${id}`} type="checkbox" />
+        <input onChange={() => handleScore({ id, value })} className="appearance-none rounded-l border-e w-1/12 cursor-pointer me-px md:me-2 checked:bg-purple-500" checked={isChecked} id={`inp-${id}`} type="checkbox" />
         <label className="p-1 cursor-pointer select-none rounded text-gray-600 inline-block w-11/12 text-sm " htmlFor={`inp-${id}`}>{ask}</label>
       </li>
     )
