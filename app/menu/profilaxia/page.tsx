03a4c3f79232f17c4dd034581c829ee8dd511f31
fischer8 <fischxr@hotmail.com>
2023-09-27 09:31:23 -0300

    dev12345

--- app/menu/profilaxia/page.tsx
+++ app/menu/profilaxia/page.tsx
@@ -10,21 +10,24 @@ export default function Profilaxia() {
       <section className="text-black absolute md:mt-26 w-full pt-10 top-0 bottom-0 bg-white">
         <section className="flex md:mt-[70px] items-center justify-center mx-auto">
           <section className="md:p-0 md:text-start w-11/12 md:w-[800px]">
-            <ul className="mt-12 mb-4 md:mt-0 text-blue-600 underline ">
+            <ul className="mt-12 mb-4 md:mt-8 text-blue-600 underline ">
               <Link className="me-4" href="/menu/profilaxia#caprini">
                 Caprini
               </Link>
               <Link className="me-4" href="/menu/profilaxia#padua">
                 Padua
               </Link>
-              <Link href="/menu/profilaxia#posologia">
+              <Link className="me-4" href="/menu/profilaxia#posologia">
                 Posologia
+              </Link>
+              <Link href="/menu/profilaxia#profilaxia">
+                Profilaxia
               </Link>
             </ul>
             <p id="posologia" className="mb-4  text-2xl">
               Posologia
             </p>
-            <section className="flex mb-10">
+            <section className="flex mb-4">
               <section className="flex p-6 text-center flex-col justify-evenly w-full bg-orange-200 border rounded">
                 <p className="mb-1 font-bold">Risco Intermediário</p>
                 <p className="text-sm">HNF 5000UI, SC 2 vezes ao dia / Enoxaparina 20mg, SC 1 vez ao dia / Dalteparina 2500UI, SC 1 vez ao dia</p>
@@ -34,7 +37,7 @@ export default function Profilaxia() {
                 <p className="text-sm">HNF 5000UI, SC 3 vezes ao dia / Enoxaparina 40mg, SC 1 vez ao dia / Dalteparina 5000UI, SC 1 vez ao dia</p>
               </section>
             </section>
-            <p className="mb-4">
+            <p className="mb-2">
               Tempo de profilaxia recomendado:
             </p>
             <section className="flex flex-col md:flex-row text-center w-full mb-12">
@@ -59,7 +62,22 @@ export default function Profilaxia() {
                 <p className="text-sm ">7 a 10 dias</p>
               </section>
             </section>
-            <p id="caprini" className="text-2xl  mb-5">
+            <p id='profilaxia' className="text-2xl mb-2">
+              Profilaxia
+            </p>
+            <p className="bg-blue-200 w-full rounded-t text-center p-2">
+              Dose para profilaxia de TEV
+            </p>
+            <section className="flex w-full mb-8 justify-evenly">
+              <p className="mb-4 p-2 py-9 bg-blue-100 rounded-bl w-full text-center">
+                Enoxaparina 40mg
+              </p>
+              <p className="mb-4 p-2 py-4 bg-orange-200 w-full rounded-ee text-center">
+              <b>Insulficiência renal<br/> (grave)</b><br/>
+                Enoxaparina 20mg
+              </p>
+            </section>
+            <p id="caprini" className="text-2xl  mb-2">
               Escore de Caprini
             </p>
             <p className="mb-5">
@@ -70,7 +88,7 @@ export default function Profilaxia() {
               Este escore foi validado em diversos tipos de pacientes cirúrgicos, abrangendo cirurgia geral, plástica, vascular, cabeça e pescoço, UTI cirúrgica, entre outros.
               Essa ferramenta ajuda os profissionais de saúde a identificar pacientes com maior risco de TVP e tomar medidas preventivas adequadas.
             </p>
-            <h2 className="md:text-2xl mb-4">
+            <h2 className="md:text-2xl mb-2">
               Profilaxia baseada no Escore de Caprini:
             </h2>
             <section className="flex flex-col mx-auto md:flex-row text-center w-full">
@@ -91,10 +109,10 @@ export default function Profilaxia() {
               <p className="mb-1 font-bold">Escore ≥5 Risco Alto</p>
               <p className="text-sm">Profilaxia farmacológica (HNF ou HBPM) + profilaxia mecânica (CPI ou meias antitrombo). Pacientes de alto risco submetidos à cirurgia oncológica abdominal e pélvica: estender a profilaxia por 4 semanas</p>
             </section>
-            <p id="padua" className="text-2xl mb-4">
+            <p id="padua" className="text-2xl mb-2">
               Escore de Padua
             </p>
-            <p className="mb-5">
+            <p className="mb-2">
               O escore de Padua pode ser aplicado em pacientes hospitalizados que apresentam risco potencial de TEV.
               O escore de predição de Pádua identifica pacientes internados que podem apresentar alto risco de tromboembolismo venoso (TEV) e que se beneficiariam de tromboprofilaxia.
             </p>
@@ -104,10 +122,10 @@ export default function Profilaxia() {
               Pacientes com contraindicações à profilaxia não foram incluídos no estudo.
               Essa ferramenta ajuda os profissionais de saúde a identificar pacientes com maior risco de TVP e tomar medidas preventivas adequadas.
             </p>
-            <h2 className="md:text-2xl mb-4">
+            <h2 className="md:text-2xl mb-2">
               Profilaxia baseada no Escore de Padua:
             </h2>
-            <section className="flex flex-col md:flex-row text-center w-full mb-2">
+            <section className="flex flex-col md:flex-row text-center w-full mb-12">
               <section className="flex flex-col px-2 justify-center w-full bg-blue-100 border rounded py-4">
                 <p className="mb-1 font-bold">Escore 0-2 Risco muito Baixo</p>
                 <p className=" text-sm">Deambulação precoce</p>
