69dbda511d161af72f36d6e56f15f9bfd5348dbd
fischer8 <fischxr@hotmail.com>
2023-09-27 09:42:30 -0300

    dev12345678910

--- components/Menu/info.tsx
+++ components/Menu/info.tsx
@@ -2,7 +2,7 @@ const cardInfo = [
   { id: 0, title: 'Cáculo de Dose', desc: 'Dose para tratamento do tromboembolismo', link: 'dose', img: '/microscopio.jpg' },
   { id: 1, title: 'Avaliação de Caprini', desc: 'Calcula o risco de TEV em pacientes cirúrgicos', link: 'caprini', img: '/avaliacao.jpg' },
   { id: 2, title: 'Avaliação de Padua', desc: 'Determina a necessidade de anticoagulante para pacientes com risco de TEV', link: 'padua', img: '/especiais.jpg' },
-  { id: 3, title: 'Profilaxia', desc: 'Orientações para profilaxia e tratamento', link: 'profilaxia', img: '/profilaxia.jpg' },
+  { id: 3, title: 'Posologia e Profilaxia', desc: 'Orientações e detalhes sobre profilaxia e tratamento', link: 'profilaxia', img: '/profilaxia.jpg' },
 ];
 
 export default cardInfo;
