38fee7708851c61d99ab92fc9060ca5640f01f24
fischer8 <fischxr@hotmail.com>
2023-10-02 21:48:07 -0300

    dev

--- components/Padua/PaduaUl.tsx
+++ components/Padua/PaduaUl.tsx
@@ -6,8 +6,8 @@ export default function PaduaUl({ handleScore, score }: PaduaProp) {
   const allQuestions = questions.map(({ id, ask, value }) => {
     const isChecked = score.some((s) => s.id === id);
     return (
-      <li className="mb-1 md:mb-0 md:hover:bg-purple-200 rounded border flex flex-row justify-between" key={id}>
-        <input onChange={() => handleScore({ id, value })} className="appearance-none rounded-l border-e w-1/12 cursor-pointer me-px md:me-2 checked:bg-purple-500" checked={isChecked} id={`inp-${id}`} type="checkbox" />
+      <li className="mb-1 md:mb-0 md:hover:border-gray-400 md:hover:bg-purple-200 rounded border border-gray-300 flex flex-row justify-between" key={id}>
+        <input onChange={() => handleScore({ id, value })} className="appearance-none hover:border-gray-400 rounded-l border-gray-300 border-e w-1/12 cursor-pointer me-px md:me-2 checked:bg-purple-500" checked={isChecked} id={`inp-${id}`} type="checkbox" />
         <label className="p-1 cursor-pointer select-none rounded text-gray-600 inline-block w-11/12 text-sm " htmlFor={`inp-${id}`}>{ask}</label>
       </li>
     )
