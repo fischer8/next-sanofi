428ce037be626bb5426fb3f3a6268a3239bd39e4
Henrique Fischer <104164663+fischer8@users.noreply.github.com>
2023-10-25 22:47:41 -0300

    Update info.tsx

--- components/Menu/info.tsx
+++ components/Menu/info.tsx
@@ -1,5 +1,5 @@
 const cardInfo = [
-  { id: 0, title: 'Cáculo de Dose', desc: 'Dose para tratamento do tromboembolismo', link: 'dose' },
+  { id: 0, title: 'Cálculo de Dose', desc: 'Dose para tratamento do tromboembolismo', link: 'dose' },
   { id: 1, title: 'Avaliação de Caprini', desc: 'Calcula o risco de TEV em pacientes cirúrgicos', link: 'caprini' },
   { id: 2, title: 'Avaliação de Padua', desc: 'Determina a necessidade de anticoagulante para pacientes com risco de TEV', link: 'padua' },
   { id: 3, title: 'Posologia e Profilaxia', desc: 'Orientações e detalhes sobre profilaxia e tratamento', link: 'profilaxia' },
